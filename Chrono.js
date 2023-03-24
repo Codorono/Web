932c7794e309bdd521b6a4a583d4d3d91b83a49a
Codorono <codorono@live.com>
2023-03-23 17:38:04 -0700

    Updated.

--- Chrono.js
+++ Chrono.js
@@ -288,7 +288,5 @@ function GetChrono()
     strToday += GetEventDays("Christmas Day", 2023, 12, 25) + " - "
 */
 
-//  document.getElementById("chrono").innerHTML = strToday
-
     return strToday
 }
