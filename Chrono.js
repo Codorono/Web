057c8bb742d0e5097287357a7cbd68e1cb781d03
Codorono <Codorono@users.noreply.github.com>
2026-07-07 16:27:43 -0700

    Updated.

--- Chrono.js
+++ Chrono.js
@@ -328,15 +328,14 @@ function GetChrono()
     strToday += "<b>Week</b> is " + nWeekOfYear + " - "
     strToday += "<b>Edge version</b> is " + strEdgeVersion + "<br />"
 
-    strToday += GetEventDays(oNow, "Independence Day", "is", 2026, 7, 4) + " - "
     strToday += GetEventDays(oNow, "Labor Day", "is", 2026, 9, 7) + " - "
     strToday += GetEventDays(oNow, "Autumn Equinox", "is", 2026, 9, 22) + " - "
     strToday += GetEventDays(oNow, "Indigenous Peoples Day", "is", 2026, 10, 12) + " - "
     strToday += GetEventDays(oNow, "Halloween", "is", 2026, 10, 31) + " - "
+    strToday += GetEventDays(oNow, "Daylight Saving Time", "ends", 2026, 11, 1) + " - "
     strToday += GetEventDays(oNow, "Election Day", "is", 2026, 11, 3)
 
 /*
-    strToday += GetEventDays(oNow, "Daylight Saving Time", "ends", 2026, 11, 1)
     strToday += GetEventDays(oNow, "Election Day", "is", 2026, 11, 3)
     strToday += GetEventDays(oNow, "Veteran's Day", "is", 2026, 11, 11)
     strToday += GetEventDays(oNow, "Thanksgiving Day", "is", 2026, 11, 26)
@@ -356,6 +355,7 @@ function GetChrono()
     strToday += GetEventDays(oNow, "Memorial Day", "is", 2027, 5, 31)
     strToday += GetEventDays(oNow, "Father's Day", "is", 2027, 6, 20)
     strToday += GetEventDays(oNow, "Summer Solstice", "is", 2027, 6, 21)
+    strToday += GetEventDays(oNow, "Independence Day", "is", 2027, 7, 4)
 */
 
     return strToday
